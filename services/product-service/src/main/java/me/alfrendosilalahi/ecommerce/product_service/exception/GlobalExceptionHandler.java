716f5ff0e1818d0c9384cc1e4722184d4ce25717
alfrendo-silalahi <alfrendos72@gmail.com>
2024-07-01 17:11:23 +0700

    create exception handler and error response dto for product service

---
+++ services/product-service/src/main/java/me/alfrendosilalahi/ecommerce/product_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package me.alfrendosilalahi.ecommerce.product_service.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import me.alfrendosilalahi.ecommerce.product_service.dto.response.ErrorResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductPurchaseException.class)
+    public ResponseEntity<String> handleCustomerNotFoundException(ProductPurchaseException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotFoundException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String fieldValue = error.getDefaultMessage();
+                    errors.put(fieldName, fieldValue);
+                });
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDTO(errors));
+    }
+
+}
